b6784145894af528998a85cabc50854cdfbc5bc7
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2025-07-21 11:53:21 +0700

    drivers: gpu: drm: renesas: rz-du: fix screen flickering in rz boards.
    Update DU driver, mipi_dsi, and dtb node to fix screen flickering in rzg2l and rzv2l evk.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- drivers/gpu/drm/renesas/rz-du/rzg2l_mipi_dsi.c
+++ drivers/gpu/drm/renesas/rz-du/rzg2l_mipi_dsi.c
@@ -475,8 +475,8 @@ static u32 rzg2l_mipi_dsi_link_read(struct rzg2l_mipi_dsi *dsi, u32 reg)
 }
 
 /* -----------------------------------------------------------------------------
-* Hardware Setup
-*/
+ * Hardware Setup
+ */
 
 static int rzg2l_mipi_dsi_dphy_init(struct rzg2l_mipi_dsi *dsi,
 					unsigned long hsfreq)
@@ -493,13 +493,13 @@ static int rzg2l_mipi_dsi_dphy_init(struct rzg2l_mipi_dsi *dsi,
 	/* All DSI global operation timings are set with recommended setting */
 	for (i = 0; i < ARRAY_SIZE(rzg2l_mipi_dsi_global_timings); ++i) {
 		dphy_timings = &rzg2l_mipi_dsi_global_timings[i];
-		if (hsfreq <= (dphy_timings->hsfreq_max * 1000))
+		if (hsfreq <= dphy_timings->hsfreq_max)
 			break;
 	}
 
 	/* Initializing DPHY before accessing LINK */
 	dphyctrl0 = DSIDPHYCTRL0_CAL_EN_HSRX_OFS | DSIDPHYCTRL0_CMN_MASTER_EN |
-			DSIDPHYCTRL0_RE_VDD_DETVCCQLV18 | DSIDPHYCTRL0_EN_BGR;
+				DSIDPHYCTRL0_RE_VDD_DETVCCQLV18 | DSIDPHYCTRL0_EN_BGR;
 
 	rzg2l_mipi_dsi_phy_write(dsi, DSIDPHYCTRL0, dphyctrl0);
 	usleep_range(20, 30);
@@ -509,19 +509,19 @@ static int rzg2l_mipi_dsi_dphy_init(struct rzg2l_mipi_dsi *dsi,
 	usleep_range(10, 20);
 
 	dphytim0 = DSIDPHYTIM0_TCLK_MISS(0) |
-		DSIDPHYTIM0_T_INIT(dphy_timings->t_init);
+		   DSIDPHYTIM0_T_INIT(dphy_timings->t_init);
 	dphytim1 = DSIDPHYTIM1_THS_PREPARE(dphy_timings->ths_prepare) |
-		DSIDPHYTIM1_TCLK_PREPARE(dphy_timings->tclk_prepare) |
-		DSIDPHYTIM1_THS_SETTLE(0) |
-		DSIDPHYTIM1_TCLK_SETTLE(0);
+		   DSIDPHYTIM1_TCLK_PREPARE(dphy_timings->tclk_prepare) |
+		   DSIDPHYTIM1_THS_SETTLE(0) |
+		   DSIDPHYTIM1_TCLK_SETTLE(0);
 	dphytim2 = DSIDPHYTIM2_TCLK_TRAIL(dphy_timings->tclk_trail) |
-		DSIDPHYTIM2_TCLK_POST(dphy_timings->tclk_post) |
-		DSIDPHYTIM2_TCLK_PRE(dphy_timings->tclk_pre) |
-		DSIDPHYTIM2_TCLK_ZERO(dphy_timings->tclk_zero);
+		   DSIDPHYTIM2_TCLK_POST(dphy_timings->tclk_post) |
+		   DSIDPHYTIM2_TCLK_PRE(dphy_timings->tclk_pre) |
+		   DSIDPHYTIM2_TCLK_ZERO(dphy_timings->tclk_zero);
 	dphytim3 = DSIDPHYTIM3_TLPX(dphy_timings->tlpx) |
-		DSIDPHYTIM3_THS_EXIT(dphy_timings->ths_exit) |
-		DSIDPHYTIM3_THS_TRAIL(dphy_timings->ths_trail) |
-		DSIDPHYTIM3_THS_ZERO(dphy_timings->ths_zero);
+		   DSIDPHYTIM3_THS_EXIT(dphy_timings->ths_exit) |
+		   DSIDPHYTIM3_THS_TRAIL(dphy_timings->ths_trail) |
+		   DSIDPHYTIM3_THS_ZERO(dphy_timings->ths_zero);
 
 	rzg2l_mipi_dsi_phy_write(dsi, DSIDPHYTIM0, dphytim0);
 	rzg2l_mipi_dsi_phy_write(dsi, DSIDPHYTIM1, dphytim1);
@@ -688,7 +688,7 @@ static void rzv2h_mipi_dsi_dphy_exit(struct rzg2l_mipi_dsi *dsi)
 }
 
 static int rzg2l_mipi_dsi_startup(struct rzg2l_mipi_dsi *dsi,
-				const struct drm_display_mode *mode)
+				  const struct drm_display_mode *mode)
 {
 	unsigned long hsfreq, vclk_rate;
 	unsigned int bpp;
@@ -708,29 +708,34 @@ static int rzg2l_mipi_dsi_startup(struct rzg2l_mipi_dsi *dsi,
 	}
 
 	/*
-	* Relationship between hsclk and vclk must follow
-	* vclk * bpp = hsclk * 8 * lanes
-	* where vclk: video clock (Hz)
-	*       bpp: video pixel bit depth
-	*       hsclk: DSI HS Byte clock frequency (Hz)
-	*       lanes: number of data lanes
-	*
-	* hsclk(bit) = hsclk(byte) * 8
-	*/
+	 * Relationship between hsclk and vclk must follow
+	 * vclk * bpp = hsclk * 8 * lanes
+	 * where vclk: video clock (Hz)
+	 *    bpp: video pixel bit depth
+	 *    hsclk: DSI HS Byte clock frequency (Hz)
+	 *    lanes: number of data lanes
+	 *
+	 * hsclk(bit) = hsclk(byte) * 8
+	 */
 	bpp = mipi_dsi_pixel_format_to_bpp(dsi->format);
-	hsfreq = (mode->clock * bpp * 8) / (8 * dsi->lanes);
+
+	if (dsi->info->type == MIPI_DSI_DPHY_RZV2H) {
+		hsfreq = DIV_ROUND_CLOSEST_ULL(vclk_rate * bpp * 8, 8 * dsi->lanes);
+	}
+	else {
+		hsfreq = (mode->clock * bpp * 8) / (8 * dsi->lanes);
+	}
 
 	ret = pm_runtime_resume_and_get(dsi->dev);
 	if (ret < 0)
 		return ret;
-
 	if (dsi->info->type == MIPI_DSI_DPHY_RZV2H) {
 		clk_set_rate(dsi->vclk, mode->clock * 1000);
-
-		ret = dsi->info->dphy_init(dsi, hsfreq);
 	} else {
-		ret = rzg2l_mipi_dsi_dphy_init(dsi, hsfreq);
+		clk_set_rate(dsi->vclk, mode->clock);
 	}
+
+	ret = dsi->info->dphy_init(dsi, hsfreq);
 	if (ret < 0)
 		goto err_phy;
 
@@ -744,19 +749,30 @@ static int rzg2l_mipi_dsi_startup(struct rzg2l_mipi_dsi *dsi,
 	}
 
 	/*
-	* Global timings characteristic depends on high speed Clock Frequency
-	* Currently MIPI DSI-IF just supports maximum FHD@60 with:
-	* - videoclock = 148.5 (MHz)
-	* - bpp: maximum 24bpp
-	* - data lanes: maximum 4 lanes
-	* Therefore maximum hsclk will be 891 Mbps.
-	*/
-	if (hsfreq > 445500000) {
+	 * Global timings characteristic depends on high speed Clock Frequency
+	 * Currently MIPI DSI-IF just supports maximum FHD@60 with:
+	 * - videoclock = 148.5 (MHz)
+	 * - bpp: maximum 24bpp
+	 * - data lanes: maximum 4 lanes
+	 * Therefore maximum hsclk will be 891 Mbps.
+	 */
+	u32	hsfreq_high_limit = 445500;
+	u32	hsfreq_mid_limit = 250000;
+
+	if (dsi->info->type == MIPI_DSI_DPHY_RZV2H) {
+		/*
+		 * For RZV2H, hsfreq is in Hz, so multiply by 1000.
+		 */
+		hsfreq_high_limit = 445500 * 1000;
+		hsfreq_mid_limit = 250000 * 1000;
+	}
+
+	if (hsfreq > hsfreq_high_limit) {
 		clkkpt = 12;
 		clkbfht = 15;
 		clkstpt = 48;
 		golpbkt = 75;
-	} else if (hsfreq > 250000000) {
+	} else if (hsfreq > hsfreq_mid_limit) {
 		clkkpt = 7;
 		clkbfht = 8;
 		clkstpt = 27;
@@ -769,7 +785,7 @@ static int rzg2l_mipi_dsi_startup(struct rzg2l_mipi_dsi *dsi,
 	}
 
 	clstptsetr = CLSTPTSETR_CLKKPT(clkkpt) | CLSTPTSETR_CLKBFHT(clkbfht) |
-			CLSTPTSETR_CLKSTPT(clkstpt);
+		CLSTPTSETR_CLKSTPT(clkstpt);
 	rzg2l_mipi_dsi_link_write(dsi, CLSTPTSETR, clstptsetr);
 
 	lptrnstsetr = LPTRNSTSETR_GOLPBKT(golpbkt);
@@ -791,7 +807,7 @@ static void rzg2l_mipi_dsi_stop(struct rzg2l_mipi_dsi *dsi)
 }
 
 static void rzg2l_mipi_dsi_set_display_timing(struct rzg2l_mipi_dsi *dsi,
-						const struct drm_display_mode *mode)
+					 const struct drm_display_mode *mode)
 {
 	u32 vich1ppsetr;
 	u32 vich1vssetr;
@@ -824,20 +840,20 @@ static void rzg2l_mipi_dsi_set_display_timing(struct rzg2l_mipi_dsi *dsi,
 
 	/* Configuration for Video Parameters */
 	vich1vssetr = VICH1VSSETR_VACTIVE(mode->vdisplay) |
-			VICH1VSSETR_VSA(mode->vsync_end - mode->vsync_start);
+		 VICH1VSSETR_VSA(mode->vsync_end - mode->vsync_start);
 	vich1vssetr |= (mode->flags & DRM_MODE_FLAG_PVSYNC) ?
 			VICH1VSSETR_VSPOL_HIGH : VICH1VSSETR_VSPOL_LOW;
 
 	vich1vpsetr = VICH1VPSETR_VFP(mode->vsync_start - mode->vdisplay) |
-			VICH1VPSETR_VBP(mode->vtotal - mode->vsync_end);
+		 VICH1VPSETR_VBP(mode->vtotal - mode->vsync_end);
 
 	vich1hssetr = VICH1HSSETR_HACTIVE(mode->hdisplay) |
-			VICH1HSSETR_HSA(mode->hsync_end - mode->hsync_start);
+		 VICH1HSSETR_HSA(mode->hsync_end - mode->hsync_start);
 	vich1hssetr |= (mode->flags & DRM_MODE_FLAG_PHSYNC) ?
 			VICH1HSSETR_HSPOL_HIGH : VICH1HSSETR_HSPOL_LOW;
 
 	vich1hpsetr = VICH1HPSETR_HFP(mode->hsync_start - mode->hdisplay) |
-			VICH1HPSETR_HBP(mode->htotal - mode->hsync_end);
+		 VICH1HPSETR_HBP(mode->htotal - mode->hsync_end);
 
 	rzg2l_mipi_dsi_link_write(dsi, VICH1VSSETR, vich1vssetr);
 	rzg2l_mipi_dsi_link_write(dsi, VICH1VPSETR, vich1vpsetr);
@@ -845,10 +861,10 @@ static void rzg2l_mipi_dsi_set_display_timing(struct rzg2l_mipi_dsi *dsi,
 	rzg2l_mipi_dsi_link_write(dsi, VICH1HPSETR, vich1hpsetr);
 
 	/*
-	* Configuration for Delay Value
-	* Delay value based on 2 ranges of video clock.
-	* 74.25MHz is videoclock of HD@60p or FHD@30p
-	*/
+	 * Configuration for Delay Value
+	 * Delay value based on 2 ranges of video clock.
+	 * 74.25MHz is videoclock of HD@60p or FHD@30p
+	 */
 	if (mode->clock > 74250) {
 		delay[0] = 231;
 		delay[1] = 216;
@@ -863,7 +879,7 @@ static void rzg2l_mipi_dsi_set_display_timing(struct rzg2l_mipi_dsi *dsi,
 		index = 1;
 
 	rzg2l_mipi_dsi_link_write(dsi, VICH1SET1R,
-				VICH1SET1R_DLY(delay[index]));
+				  VICH1SET1R_DLY(delay[index]));
 }
 
 static int rzg2l_mipi_dsi_start_hs_clock(struct rzg2l_mipi_dsi *dsi)
@@ -877,8 +893,8 @@ static int rzg2l_mipi_dsi_start_hs_clock(struct rzg2l_mipi_dsi *dsi)
 
 	/* Start HS clock */
 	hsclksetr = HSCLKSETR_HSCLKRUN_HS | (is_clk_cont ?
-						HSCLKSETR_HSCLKMODE_CONT :
-						HSCLKSETR_HSCLKMODE_NON_CONT);
+					HSCLKSETR_HSCLKMODE_CONT :
+					HSCLKSETR_HSCLKMODE_NON_CONT);
 	rzg2l_mipi_dsi_link_write(dsi, HSCLKSETR, hsclksetr);
 
 	if (is_clk_cont) {
@@ -907,8 +923,8 @@ static int rzg2l_mipi_dsi_stop_hs_clock(struct rzg2l_mipi_dsi *dsi)
 
 	/* Stop HS clock */
 	rzg2l_mipi_dsi_link_write(dsi, HSCLKSETR,
-				is_clk_cont ? HSCLKSETR_HSCLKMODE_CONT :
-				HSCLKSETR_HSCLKMODE_NON_CONT);
+				  is_clk_cont ? HSCLKSETR_HSCLKMODE_CONT :
+				  HSCLKSETR_HSCLKMODE_NON_CONT);
 
 	if (is_clk_cont) {
 		ret = read_poll_timeout(rzg2l_mipi_dsi_link_read, status,
@@ -931,7 +947,7 @@ static int rzg2l_mipi_dsi_start_video(struct rzg2l_mipi_dsi *dsi)
 
 	/* Configuration for Blanking sequence and start video input*/
 	vich1set0r = VICH1SET0R_HFPNOLP | VICH1SET0R_HBPNOLP |
-			VICH1SET0R_HSANOLP | VICH1SET0R_VSTART;
+		VICH1SET0R_HSANOLP | VICH1SET0R_VSTART;
 	rzg2l_mipi_dsi_link_write(dsi, VICH1SET0R, vich1set0r);
 
 	ret = read_poll_timeout(rzg2l_mipi_dsi_link_read, status,
@@ -969,11 +985,11 @@ err:
 }
 
 /* -----------------------------------------------------------------------------
-* Bridge
-*/
+ * Bridge
+ */
 
 static int rzg2l_mipi_dsi_attach(struct drm_bridge *bridge,
-				enum drm_bridge_attach_flags flags)
+				 enum drm_bridge_attach_flags flags)
 {
 	struct rzg2l_mipi_dsi *dsi = bridge_to_rzg2l_mipi_dsi(bridge);
 
@@ -982,7 +998,7 @@ static int rzg2l_mipi_dsi_attach(struct drm_bridge *bridge,
 }
 
 static void rzg2l_mipi_dsi_atomic_enable(struct drm_bridge *bridge,
-					struct drm_bridge_state *old_bridge_state)
+					 struct drm_bridge_state *old_bridge_state)
 {
 	struct drm_atomic_state *state = old_bridge_state->base.state;
 	struct rzg2l_mipi_dsi *dsi = bridge_to_rzg2l_mipi_dsi(bridge);
@@ -1018,7 +1034,7 @@ err_stop:
 }
 
 static void rzg2l_mipi_dsi_atomic_disable(struct drm_bridge *bridge,
-					struct drm_bridge_state *old_bridge_state)
+					  struct drm_bridge_state *old_bridge_state)
 {
 	struct rzg2l_mipi_dsi *dsi = bridge_to_rzg2l_mipi_dsi(bridge);
 
@@ -1030,8 +1046,8 @@ static void rzg2l_mipi_dsi_atomic_disable(struct drm_bridge *bridge,
 
 static enum drm_mode_status
 rzg2l_mipi_dsi_bridge_mode_valid(struct drm_bridge *bridge,
-				const struct drm_display_info *info,
-				const struct drm_display_mode *mode)
+				 const struct drm_display_info *info,
+				 const struct drm_display_mode *mode)
 {
 	if (mode->clock > 148500)
 		return MODE_CLOCK_HIGH;
@@ -1050,8 +1066,8 @@ static const struct drm_bridge_funcs rzg2l_mipi_dsi_bridge_ops = {
 };
 
 /* -----------------------------------------------------------------------------
-* Host setting
-*/
+ * Host setting
+ */
 
 int rzg2l_mipi_dsi_get_data_lanes(struct platform_device *pdev)
 {
@@ -1070,7 +1086,7 @@ int rzg2l_mipi_dsi_get_bpp(struct platform_device *pdev)
 EXPORT_SYMBOL_GPL(rzg2l_mipi_dsi_get_bpp);
 
 static int rzg2l_mipi_dsi_host_attach(struct mipi_dsi_host *host,
-					struct mipi_dsi_device *device)
+				 struct mipi_dsi_device *device)
 {
 	struct rzg2l_mipi_dsi *dsi = host_to_rzg2l_mipi_dsi(host);
 	int ret;
@@ -1101,7 +1117,7 @@ static int rzg2l_mipi_dsi_host_attach(struct mipi_dsi_host *host,
 	dsi->mode_flags = device->mode_flags;
 
 	dsi->next_bridge = devm_drm_of_get_bridge(dsi->dev, dsi->dev->of_node,
-						1, 0);
+						  1, 0);
 	if (IS_ERR(dsi->next_bridge)) {
 		ret = PTR_ERR(dsi->next_bridge);
 		dev_err(dsi->dev, "failed to get next bridge: %d\n", ret);
@@ -1114,7 +1130,7 @@ static int rzg2l_mipi_dsi_host_attach(struct mipi_dsi_host *host,
 }
 
 static int rzg2l_mipi_dsi_host_detach(struct mipi_dsi_host *host,
-					struct mipi_dsi_device *device)
+				 struct mipi_dsi_device *device)
 {
 	struct rzg2l_mipi_dsi *dsi = host_to_rzg2l_mipi_dsi(host);
 
@@ -1129,8 +1145,8 @@ static const struct mipi_dsi_host_ops rzg2l_mipi_dsi_host_ops = {
 };
 
 /* -----------------------------------------------------------------------------
-* Power Management
-*/
+ * Power Management
+ */
 
 static int __maybe_unused rzg2l_mipi_pm_runtime_suspend(struct device *dev)
 {
@@ -1163,8 +1179,8 @@ static const struct dev_pm_ops rzg2l_mipi_pm_ops = {
 };
 
 /* -----------------------------------------------------------------------------
-* Probe & Remove
-*/
+ * Probe & Remove
+ */
 
 static int rzg2l_mipi_dsi_probe(struct platform_device *pdev)
 {
@@ -1188,7 +1204,7 @@ static int rzg2l_mipi_dsi_probe(struct platform_device *pdev)
 	ret = drm_of_get_data_lanes_count_ep(dsi->dev->of_node, 1, 0, 1, 4);
 	if (ret < 0)
 		return dev_err_probe(dsi->dev, ret,
-					"missing or invalid data-lanes property\n");
+				"missing or invalid data-lanes property\n");
 
 	num_data_lanes = ret;
 
@@ -1208,18 +1224,18 @@ static int rzg2l_mipi_dsi_probe(struct platform_device *pdev)
 		dsi->rstc = devm_reset_control_get_exclusive(dsi->dev, "rst");
 		if (IS_ERR(dsi->rstc))
 			return dev_err_probe(dsi->dev, PTR_ERR(dsi->rstc),
-						"failed to get rst\n");
+					"failed to get rst\n");
 	}
 
 	dsi->arstc = devm_reset_control_get_exclusive(dsi->dev, "arst");
 	if (IS_ERR(dsi->arstc))
 		return dev_err_probe(&pdev->dev, PTR_ERR(dsi->arstc),
-					"failed to get arst\n");
+				"failed to get arst\n");
 
 	dsi->prstc = devm_reset_control_get_exclusive(dsi->dev, "prst");
 	if (IS_ERR(dsi->prstc))
 		return dev_err_probe(dsi->dev, PTR_ERR(dsi->prstc),
-					"failed to get prst\n");
+				"failed to get prst\n");
 
 	platform_set_drvdata(pdev, dsi);
 
@@ -1235,11 +1251,16 @@ static int rzg2l_mipi_dsi_probe(struct platform_device *pdev)
 	}
 
 	/*
-	* TXSETR register can be read only after DPHY init. But during probe
-	* mode->clock and format are not available. So initialize DPHY with
-	* timing parameters for 80Mbps.
-	*/
-	ret = dsi->info->dphy_init(dsi, 80000000);
+	 * TXSETR register can be read only after DPHY init. But during probe
+	 * mode->clock and format are not available. So initialize DPHY with
+	 * timing parameters for 80Mbps.
+	 */
+	unsigned long init_clock = 80000;
+	if (dsi->info->type == MIPI_DSI_DPHY_RZV2H) {
+		init_clock = 80000000;
+	}
+
+	ret = dsi->info->dphy_init(dsi, init_clock);
 	if (ret < 0)
 		goto err_phy;
 
