64328253e0e3314017efee77f017c60a3a48e023
Long Trinh <long.trinh.rh@bp.renesas.com>
2026-01-14 17:07:39 +0700

    rz-cmn: Support RZV2H-EVK SDHI/MMC
    Signed-off-by: Long Trinh <long.trinh.rh@bp.renesas.com>

--- drivers/mmc/host/renesas_sdhi.h
+++ drivers/mmc/host/renesas_sdhi.h
@@ -97,14 +97,17 @@ struct renesas_sdhi {
 	struct reset_control *rstc;
 	struct tmio_mmc_host *host;
 	struct regulator_dev *rdev;
+
+	/* Pin Voltage Switching support: 1 for not support, 0 for support */
+	bool no_pin_volt_switch;
 };
 
 #define host_to_priv(host) \
 	container_of((host)->pdata, struct renesas_sdhi, mmc_data)
 
 int renesas_sdhi_probe(struct platform_device *pdev,
-		       const struct tmio_mmc_dma_ops *dma_ops,
-		       const struct renesas_sdhi_of_data *of_data,
-		       const struct renesas_sdhi_quirks *quirks);
+			const struct tmio_mmc_dma_ops *dma_ops,
+			const struct renesas_sdhi_of_data *of_data,
+			const struct renesas_sdhi_quirks *quirks);
 void renesas_sdhi_remove(struct platform_device *pdev);
 #endif
