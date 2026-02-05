54cf1145a13855ec22dd5533f4992e08c8f64098
Tien Nguyen <tien.nguyen.uh@renesas.com>
2026-02-05 09:26:28 +0000

    ASoC: da7219: set PLL to SRM mode
    Enable SRM (Sample Rate Matching) mode by updating the default PLL_CTRL
    value in the mic enable register patch.
    
    Signed-off-by: Tien Nguyen <tien.nguyen.uh@renesas.com>

--- sound/soc/codecs/da7219.c
+++ sound/soc/codecs/da7219.c
@@ -2452,7 +2452,7 @@ static struct reg_sequence da7219_rev_aa_patch[] = {
 };
 
 static struct reg_sequence da7219_mic_enable_patch[] = {
-	{ DA7219_PLL_CTRL, 0x08 },
+	{ DA7219_PLL_CTRL, 0x88 },
 	{ DA7219_DAI_TDM_CTRL, 0x40 },
 	{ DA7219_MIXIN_L_GAIN, 0x0B },
 	{ DA7219_ADC_L_GAIN, 0x5F },
